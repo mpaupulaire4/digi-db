2d1362fd0de2ddece8982a675a148bd6921b53b8
Michel Pupulaire <mpaupulaire4@gmail.com>
2021-08-01 18:09:49 -0400

    save

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
