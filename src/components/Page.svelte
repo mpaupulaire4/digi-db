6f5b3a3519b0e6e1b085b0efe50d4f857d2100ab
Michel Pupulaire <mpaupulaire4@gmail.com>
2021-08-03 11:47:13 -0400

    save

--- src/components/Page.svelte
+++ src/components/Page.svelte
@@ -1,9 +1,7 @@
 {#if $$slots.header}
 <header class="bg-white shadow z-10">
-  <div class="max-w-7xl mx-auto py-4 px-4 sm:px-6 lg:px-8">
-    <h1 class="text-lg leading-6 font-semibold text-gray-900 flex">
-      <slot name="header"/>
-    </h1>
+  <div class="max-w-7xl mx-auto py-4 px-4 sm:px-6 lg:px-8 flex">
+    <slot name="header"/>
   </div>
 </header>
 {/if}
