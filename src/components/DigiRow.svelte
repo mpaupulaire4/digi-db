3f7d4c33d46af52f7609323740fb0189716493af
Michel Paupulaire <mpaupulaire4@gmail.com>
2021-08-21 08:17:07 -0400

    save

--- src/components/DigiRow.svelte
+++ src/components/DigiRow.svelte
@@ -1,21 +1,29 @@
 <script>
+  import TypeSymbol from './TypeSymbol.svelte'
+
   export let digimon;
+
+  console.log(digimon.images);
 </script>
 
 <div
   class="relative z-0 shadow-md bg-gray-700 rounded-xl flex overflow-hidden w-full"
 >
-  <div class="flex-1 p-2 px-4 text-gray-100">
-      <!-- <span class="font-mono font-medium capitalize text-sm mr-2">#{digimon.id.toString().padStart(3, '0')}</span> -->
-    <span class="text-lg leading-6 font-bold capitalize tracking-wide" >{digimon.name}</span>
-      <!-- <span class="font-mono font-medium capitalize text-sm ml-2">{digimon.stage}</span> -->
-      <br/><span class="font-mono font-medium capitalize text-sm">#{digimon.id.toString().padStart(3, '0')} - {digimon.stage}</span>
+  <div class="flex-1 p-2 text-gray-100 space-y-2">
+    <div class="text-lg font-bold capitalize text-center" >{digimon.name}</div>
 
-      <br/>
-      <span>{digimon.type}</span>
-    <span>{digimon.attribute}</span>
+    <div class="border-gray-100 rounded-full border-2 flex flex-1 items-center overflow-hidden">
+      <div class="bg-gray-100 h-full flex items-center justify-center px-2 rounded-full overflow-hidden">
+        <TypeSymbol type="{digimon.type}" class="w-4 h-4 text-gray-800" />
+        <span class="sr-only">{digimon.type} type</span>
+      </div>
+      <div class="capitalize text-xs flex-1 text-center font-bold tracking-widest">{digimon.stage}</div>
+      <div class="bg-gray-100 h-full flex items-center justify-center px-2 rounded-full">
+        <img src="/images/types/{digimon.attribute}-icon.png" class="w-4 h-4" alt="{digimon.attribute} attribute" />
+      </div>
+    </div>
   </div>
-  <div class="bg-gray-500 p-2  rounded-l-xl">
+  <div class="bg-gray-500 p-2 rounded-l-xl">
     <img
       src="/{digimon.images.icon}"
       alt="{digimon.name}"
